59cfc25a1fa32fcbfc6374f06618a9d12152f7e2
The XOR <1h3x0r@gmail.com>
2025-11-20 15:42:26 +0100

    mtc

--- MTCDisplay/MTCDisplay.ino
+++ MTCDisplay/MTCDisplay.ino
@@ -16,7 +16,7 @@ USBMIDI_CREATE_DEFAULT_INSTANCE();
 #define DIN 7
 #define CS 6
 #define CLK 5
-#define BANNER "thE xOR "
+#define BANNER "tHE XOR "
 
 DigitLedDisplay ld = DigitLedDisplay(8, DIN, CS, CLK);
 char toDisp[20];
@@ -25,6 +25,8 @@ volatile unsigned long lastMsg;
 volatile bool midiclock_enabled;
 volatile uint16_t songPosition;
 volatile bool transportRunning;
+volatile uint16_t signature_numerator;
+volatile uint16_t signature_denominator;
 
 enum
 {
@@ -61,19 +63,19 @@ void handleTimeCodeQuarterFrame(byte data)
 		byte fmax;
 		switch (frameType)
 		{
-		case F24:
-			fmax = 24;
-			break;
-		case F25:
-			fmax = 25;
-			break;
-		case F30DF:
-		case F30:
-			fmax = 30;
-			break;
-		default:
-			h = m = s = f = 8; // ERROR!
-			break;
+			case F24:
+				fmax = 24;
+				break;
+			case F25:
+				fmax = 25;
+				break;
+			case F30DF:
+			case F30:
+				fmax = 30;
+				break;
+			default:
+				h = m = s = f = 8; // ERROR!
+				break;
 		}
 		if (f > fmax)
 			f = fmax;
@@ -124,6 +126,34 @@ void handleSongPosition(uint16_t beats)
 	updateMidiClockDisplay();
 }
 
+void handleControlChange(byte channel, byte control, byte value)
+{
+	if(midiclock_enabled)
+	{
+		lastMsg = millis();
+		switch(control)
+		{
+			case 0x59: // Time Signature: denominatore
+				if(value > 0 && value <= 16)
+				{
+					signature_denominator = value; 
+					songPosition = 0;
+					sprintf(toDisp, "S: %d/%d", signature_numerator, signature_denominator);	
+				}
+				break;
+
+			case 0x58: // Time Signature: numeratore
+				if(value > 0 && value <= 16)
+				{
+					signature_numerator = value; // Numerator
+					songPosition = 0;
+					sprintf(toDisp, "S: %d/%d", signature_numerator, signature_denominator);	
+				}
+				break;
+		}
+	}
+}
+
 void handleProgramChange(byte channel, byte program) 
 {
 	if(program == 127)  // PC 127 ---> reset generale (cosi', a gradire, se dovesse servire...)
@@ -134,22 +164,46 @@ void updateMidiClockDisplay()
 {
 	if(midiclock_enabled)
 	{
-		// la si presuppone la non comune misuranza di 4/4
-	  uint16_t m = songPosition / 96;
-    uint8_t b = (songPosition % 96) / 24;
-    uint8_t s = songPosition % 24;
-		sprintf(toDisp, "%02d %02d %02d", m+1, b+1, s);
+		// Compute clocks per beat and per measure using the current time signature.
+		// MIDI has 24 clocks per quarter note, so clocks per beat = 96 / denominator.
+	  	
+		uint16_t total_beats = (songPosition / 24) * signature_denominator);
+		uint16_t m = 1+(total_beats / signature_numerator);
+
+		uint16_t clocks_per_beat = 96 / signature_denominator; // 96 = 24 * 4
+		if(clocks_per_beat == 0) 
+			return;
+
+		uint16_t clocks_per_measure = clocks_per_beat * signature_numerator;
+		if(clocks_per_measure == 0) 
+			return;
+
+		uint16_t rem = songPosition % clocks_per_measure
+		uint16_t b = rem / clocks_per_beat;
+		uint16_t s = rem % clocks_per_beat;
+		if(m < 100)
+			sprintf(toDisp, "%02lu %02lu %02lu", m, b+1, s);
+		else
+			sprintf(toDisp, "%03lu.%02lu %02lu", m, b+1, s);
 	}
+}
+
+void clr()
+{
+	memset(toDisp, 0, sizeof(toDisp));
+	strcpy(toDisp, BANNER);
 }
 
 void initialize()
 {
-	strcpy(toDisp, BANNER);
+	clr();	
 	lastSMPTETime = 0;
 	lastMsg = millis();
 	midiclock_enabled = false;
 	transportRunning = false;
 	songPosition = 0;
+	signature_numerator = 4;
+	signature_denominator = 4;
 }
 
 void setup()
@@ -161,30 +215,37 @@ void setup()
 	MIDI.setHandleStart(handleStart);
 	MIDI.setHandleContinue(handleContinue);
 	MIDI.setHandleStop(handleStop);
-  MIDI.setHandleProgramChange(handleProgramChange);
+	MIDI.setHandleReset(handleReset);
+  	MIDI.setHandleProgramChange(handleProgramChange);
+  	MIDI.setHandleControlChange(handleControlChange);
 	MIDI.setHandleSongPosition(handleSongPosition);
 
 	initialize();
 	MIDI.begin(1); // listen channel 1, ma tanto non e' usato
 	#ifdef SERIAL_DEBUG
 	Serial.begin(115200);
-  Serial.println("Debug abilitato");
+  	Serial.println("Debug abilitato");
 	#endif
 }
 
 void loop()
 {
 	MIDI.read();
-	if (toDisp[0])
+	if(toDisp[0])
 	{
 		ld.printString(toDisp);
-		toDisp[0] = 0;
+		memset(toDisp, 0, sizeof(toDisp));
 	}
 
-	midiclock_enabled = (millis() - lastSMPTETime > 10000); // se non si riceve un casso entro 10 secondi, abilita la POSSIBILE riceziun del midiclock. Altrimenti, SMPTE got the prece/dance.	
+	bool test_mclock = (millis() - lastSMPTETime > 10000); // se non si riceve un casso entro 10 secondi, abilita la POSSIBILE riceziun del midiclock. Altrimenti, SMPTE got the prece/dance.	
+	if(test_mclock != midiclock_enabled)
+	{
+		clr();	
+		midiclock_enabled = test_mclock;
+	} 
+	
 	if((millis() - lastMsg) > 1200000) // se non si riceve lo stesso casso di cui sopra per 20 minuti, o ci siamo addormentati, o siamo morti o molto piu' probabilmente ubriachi come la merda
 	{
-		strcpy(toDisp, BANNER);
-		lastMsg = millis();
+		clr();	
 	}		
 }
